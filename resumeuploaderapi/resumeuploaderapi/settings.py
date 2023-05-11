82beee3da17a2f751fbd23d9b256c3f6a0f1ddd8
Akanksha Kriti <akankshakriti09@gmail.com>
2023-05-11 17:34:42 +0530

    Configuration CORS_ALLOWED_ORIGINS

--- resumeuploaderapi/resumeuploaderapi/settings.py
+++ resumeuploaderapi/resumeuploaderapi/settings.py
@@ -128,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+]
