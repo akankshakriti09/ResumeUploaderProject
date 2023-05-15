1d4b526872799c4ac36f82296b3d32959e8cedf2
Akanksha Kriti <akankshakriti09@gmail.com>
2023-05-15 14:22:25 +0530

    cmtd

--- resumeuploaderui/src/App.js
+++ resumeuploaderui/src/App.js
@@ -1,34 +1,62 @@
-import { Grid, TextField, Typography, FormControlLabel, Checkbox, Button, Box, Alert, InputLabel, MenuItem, Select, FormControl, FormLabel, RadioGroup, Radio, FormGroup, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Avatar } from '@mui/material';
-import { LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
+import {
+  Grid,
+  TextField,
+  Typography,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Box,
+  Alert,
+  InputLabel,
+  MenuItem,
+  Select,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  FormGroup,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Avatar,
+} from "@mui/material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { useEffect, useState } from 'react';
-import { styled } from '@mui/material/styles';
-import {  useSaveProfileMutation, useGetResumeProfileQuery } from './services/candidateProfileApi';
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import {
+  useSaveProfileMutation,
+  useGetResumeProfileQuery,
+} from "./services/candidateProfileApi";
 import { format } from "date-fns";
 
 function App() {
-
   // Style for Upload Button
-  const Input = styled('input')({
-    display: 'none',
+  const Input = styled("input")({
+    display: "none",
   });
 
   // States
-  const [name, setName] = useState() //Name
-  const [email, setEmail] = useState() //EMail
-  const [dob, setDob] = useState(null) //Date of Birth
-  const [st, setSt] = useState('') //State
-  const [gender, setGender] = useState() //Gender
-  const [pjl, setPjl] = useState([]) //Job Location
-  const [pimage, setPimage] = useState('') //Upload Image
-  const [rdoc, setRdoc] = useState('') //Upload Resume Doc
+  const [name, setName] = useState(); //Name
+  const [email, setEmail] = useState(); //EMail
+  const [dob, setDob] = useState(null); //Date of Birth
+  const [st, setSt] = useState(""); //State
+  const [gender, setGender] = useState(); //Gender
+  const [pjl, setPjl] = useState([]); //Job Location
+  const [pimage, setPimage] = useState(""); //Upload Image
+  const [rdoc, setRdoc] = useState(""); //Upload Resume Doc
   const [error, setError] = useState({
     status: false,
     msg: "",
-    type: ""
-  })
-  const [candidates, setCandidates] = useState([])
+    type: "",
+  });
+  const [candidates, setCandidates] = useState([]);
 
   // Multi Checkbox
   const getPjl = (e) => {
@@ -37,157 +65,288 @@ function App() {
     console.log(`${value} is ${checked}`);
     // User checks the box
     if (checked) {
-      setPjl([...pjl, value])
+      setPjl([...pjl, value]);
     }
     // User unchecks the box
     else {
-      setPjl(pjl.filter((e) => e !== value))
+      setPjl(pjl.filter((e) => e !== value));
     }
-  }
+  };
 
   // Clear Form
   const resetForm = () => {
-    setName('')
-    setEmail('')
-    setDob(null)
-    setSt('')
-    setGender('')
-    setPjl([])
-    setPimage('')
-    setRdoc('')
-    document.getElementById('resume-form').reset()
-  }
+    setName("");
+    setEmail("");
+    setDob(null);
+    setSt("");
+    setGender("");
+    setPjl([]);
+    setPimage("");
+    setRdoc("");
+    document.getElementById("resume-form").reset();
+  };
 
   // RTK Query
-  const [saveProfile] = useSaveProfileMutation()
-  const { data, isSuccess } = useGetResumeProfileQuery()
+  const [saveProfile] = useSaveProfileMutation();
+  const { data, isSuccess, getResumeProfile } = useGetResumeProfileQuery();
 
-  useEffect(() => {
-    if (data && isSuccess) {
-      setCandidates(data.candidates)
-    }
-  }, [data, isSuccess])
+  useEffect(async() => {
+    const response = await fetch('http://127.0.0.1:8000/api');
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }, []);
 
 
   // Handle Form Submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = new FormData()
-    data.append('name', name)
-    data.append('email', email)
-    data.append('dob', dob==null? null: format(dob,'yyyy-MM-dd'))
-    data.append('state', st)
-    data.append('gender', gender)
-    data.append('location', pjl)
-    data.append('pimage', pimage)
-    data.append('rdoc', rdoc)
+    const data = new FormData();
+    data.append("name", name);
+    data.append("email", email);
+    data.append(
+      "dob",
+      dob == null ? null : format(new Date(dob), "yyyy-MM-dd")
+    );
+    data.append("state", st);
+    data.append("gender", gender);
+    data.append("location", pjl);
+    data.append("pimage", pimage);
+    data.append("rdoc", rdoc);
     if (name && email) {
-      const res = await saveProfile(data)
+      const res = await saveProfile(data);
+      console.log(res);
       if (res.data.status === "success") {
-        setError({ status: true, msg: "Resume Uploaded Successfully", type: 'success' })
-        resetForm()
+        setError({
+          status: false,
+          msg: "Resume Uploaded Successfully",
+          type: "success",
+        });
+        resetForm();
       }
     } else {
-      setError({ status: true, msg: "All Fields are Required", type: 'error' })
+      setError({ status: true, msg: "All Fields are Required", type: "error" });
     }
-  }
+  };
 
   return (
     <>
-      <Box display="flex" justifyContent="center" sx={{
-       backgroundColor:"#B71C1C" , padding:2}}>
-        <Typography variant="h4" component="div" sx={{ 
-          fontWeight:"bold", color:"white"}}>Resume Uploader</Typography>
+      <Box
+        display="flex"
+        justifyContent="center"
+        sx={{
+          backgroundColor: "#B71C1C",
+          padding: 2,
+        }}
+      >
+        <Typography
+          variant="h4"
+          component="div"
+          sx={{
+            fontWeight: "bold",
+            color: "white",
+          }}
+        >
+          Resume Uploader
+        </Typography>
       </Box>
       <Grid container justifyContent="center">
-
         <Grid item xs={5}>
-          <Box component="form" sx={{ p: 3 }} noValidate id="resume-form" onSubmit={handleSubmit}>
-            <TextField id="name" name="name" required fullWidth margin='normal' label='Name' onChange={(e) => setName(e.target.value)} />
-            <TextField id="email" email="email" required fullWidth margin='normal' label='Email' onChange={(e) => setEmail(e.target.value)} />
+          <Box
+            component="form"
+            sx={{ p: 3 }}
+            noValidate
+            id="resume-form"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              id="name"
+              name="name"
+              required
+              fullWidth
+              margin="normal"
+              label="Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              id="email"
+              email="email"
+              required
+              fullWidth
+              margin="normal"
+              label="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <Box mt={2}>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker 
-                label="Date of Birth"
-                value={dob} 
-                onChange={(newValue) => setDob(newValue)} 
-                textField={(props) => <TextField {...props} />}
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Date of Birth"
+                  value={dob}
+                  onChange={(newValue) => setDob(newValue)}
+                  textField={(props) => <TextField {...props} />}
                 />
               </LocalizationProvider>
             </Box>
-            <FormControl fullWidth margin='normal'>
+            <FormControl fullWidth margin="normal">
               <InputLabel id="state-select-label">State</InputLabel>
-              <Select labelId='state-select-label' id='state-select' value={st} label='st' onChange={(e) => { setSt(e.target.value) }}>
-              <MenuItem value="AP">Andhra Pradesh</MenuItem>
-                <MenuItem value="ARP">Arunachal Pradesh</MenuItem>
-                <MenuItem value="ASM">Assam</MenuItem>
-                <MenuItem value="BH">Bihar</MenuItem>
-                <MenuItem value="CH">Chhattisgarh</MenuItem>
-                <MenuItem value="G">Goa</MenuItem>
-                <MenuItem value="GJ">Gujarat</MenuItem>
-                <MenuItem value="JH">Jharkhand</MenuItem>
-                <MenuItem value="KA">Karnataka</MenuItem>
-                <MenuItem value="KL">Kerala</MenuItem>
-                <MenuItem value="MP">Madhya Pradesh</MenuItem>
-                <MenuItem value="MH">Maharashtra</MenuItem>
-                <MenuItem value="MR">Manipur</MenuItem>
-                <MenuItem value="MGH">Meghalaya</MenuItem>
-                <MenuItem value="MZ">Mizoram</MenuItem>
-                <MenuItem value="N">Nagaland</MenuItem>
-                <MenuItem value="O">Odisha</MenuItem>
-                <MenuItem value="P">Punjab</MenuItem>
-                <MenuItem value="RJ">Rajasthan</MenuItem>
-                <MenuItem value="S"> Sikkim</MenuItem>
-                <MenuItem value="TN">Tamil Nadu</MenuItem>
-                <MenuItem value="TL">Telangana</MenuItem>
-                <MenuItem value="TR">Tripura</MenuItem>
-                <MenuItem value="UK">Uttarakhand</MenuItem>
-                <MenuItem value="UP">Uttar Pradesh</MenuItem>
-                <MenuItem value="WB">West Bengal</MenuItem>
+              <Select
+                labelId="state-select-label"
+                id="state-select"
+                value={st}
+                label="st"
+                onChange={(e) => {
+                  setSt(e.target.value);
+                }}
+              >
+                <MenuItem value="Andhra Pradesh">Andhra Pradesh</MenuItem>
+                <MenuItem value="Arunachal Pradesh">Arunachal Pradesh</MenuItem>
+                <MenuItem value="Assam">Assam</MenuItem>
+                <MenuItem value="Bihar">Bihar</MenuItem>
+                <MenuItem value="Chhattisgarh">Chhattisgarh</MenuItem>
+                <MenuItem value="Goa">Goa</MenuItem>
+                <MenuItem value="Gujarat">Gujarat</MenuItem>
+                <MenuItem value="Jharkhand">Jharkhand</MenuItem>
+                <MenuItem value="Karnataka">Karnataka</MenuItem>
+                <MenuItem value="Kerala">Kerala</MenuItem>
+                <MenuItem value="Madhya Pradesh">Madhya Pradesh</MenuItem>
+                <MenuItem value="Maharashtra">Maharashtra</MenuItem>
+                <MenuItem value="Manipur">Manipur</MenuItem>
+                <MenuItem value="Meghalaya">Meghalaya</MenuItem>
+                <MenuItem value="Mizoram">Mizoram</MenuItem>
+                <MenuItem value="Nagaland">Nagaland</MenuItem>
+                <MenuItem value="Odisha">Odisha</MenuItem>
+                <MenuItem value="Punjab">Punjab</MenuItem>
+                <MenuItem value="Rajasthan">Rajasthan</MenuItem>
+                <MenuItem value=" Sikkim"> Sikkim</MenuItem>
+                <MenuItem value="Tamil">Tamil Nadu</MenuItem>
+                <MenuItem value="Telangana">Telangana</MenuItem>
+                <MenuItem value="Tripura">Tripura</MenuItem>
+                <MenuItem value="Uttarakhand">Uttarakhand</MenuItem>
+                <MenuItem value="Uttar Pradesh">Uttar Pradesh</MenuItem>
+                <MenuItem value="West Bengal">West Bengal</MenuItem>
               </Select>
             </FormControl>
 
-            <FormControl fullWidth margin='normal'>
+            <FormControl fullWidth margin="normal">
               <FormLabel id="gender-radio">Gender</FormLabel>
               <RadioGroup row name="gender" aria-labelledby="gender-radio">
-                <FormControlLabel value="male" control={<Radio />} label='Male' onChange={(e) => setGender(e.target.value)} />
-                <FormControlLabel value="female" control={<Radio />} label='Female' onChange={(e) => setGender(e.target.value)} />
-                <FormControlLabel value="other" control={<Radio />} label='Other' onChange={(e) => setGender(e.target.value)} />
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label="Male"
+                  onChange={(e) => setGender(e.target.value)}
+                />
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label="Female"
+                  onChange={(e) => setGender(e.target.value)}
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio />}
+                  label="Other"
+                  onChange={(e) => setGender(e.target.value)}
+                />
               </RadioGroup>
             </FormControl>
 
-            <FormControl component='fieldset' fullWidth margin='normal'>
-              <FormLabel component='legend'>Preferred Job Location:</FormLabel>
+            <FormControl component="fieldset" fullWidth margin="normal">
+              <FormLabel component="legend">Preferred Job Location:</FormLabel>
               <FormGroup row>
-              <FormControlLabel control={<Checkbox />} label="Bangalore" value="Bangalore" onChange={(e) => getPjl(e)} />
-               <FormControlLabel control={<Checkbox />} label="Hyedarabad" value="Hyedarabad" onChange={(e) => getPjl(e)} />
-               <FormControlLabel control={<Checkbox />} label="Mumbai" value="Mumbai" onChange={(e) => getPjl(e)} />
-               <FormControlLabel control={<Checkbox />} label="Delhi" value="Delhi" onChange={(e) => getPjl(e)} />
-               <FormControlLabel control={<Checkbox />} label="Kolkata" value="Kolkata" onChange={(e) => getPjl(e)} /> 
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Bangalore"
+                  value="Bangalore"
+                  onChange={(e) => getPjl(e)}
+                />
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Hyedarabad"
+                  value="Hyedarabad"
+                  onChange={(e) => getPjl(e)}
+                />
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Mumbai"
+                  value="Mumbai"
+                  onChange={(e) => getPjl(e)}
+                />
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Delhi"
+                  value="Delhi"
+                  onChange={(e) => getPjl(e)}
+                />
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Kolkata"
+                  value="Kolkata"
+                  onChange={(e) => getPjl(e)}
+                />
               </FormGroup>
             </FormControl>
 
-            <Stack direction="row" alignItems="center" spacing={4} >
-              <label htmlFor='profile-photo'>
-                <Input accept="image/*" id="profile-photo" type="file" onChange={(e) => { setPimage(e.target.files[0]) }} />
-                <Button variant='contained' component='span'>Upload Photo</Button>
+            <Stack direction="row" alignItems="center" spacing={4}>
+              <label htmlFor="profile-photo">
+                <Input
+                  accept="image/*"
+                  id="profile-photo"
+                  type="file"
+                  onChange={(e) => {
+                    setPimage(e.target.files[0]);
+                  }}
+                />
+                <Button variant="contained" component="span">
+                  Upload Photo
+                </Button>
               </label>
               <label htmlFor="resume-file">
-                <Input accept="doc/*" id="resume-file" type="file" onChange={(e) => { setRdoc(e.target.files[0]) }} />
-                <Button variant="contained" component="span">Upload File</Button>
+                <Input
+                  accept="doc/*"
+                  id="resume-file"
+                  type="file"
+                  onChange={(e) => {
+                    setRdoc(e.target.files[0]);
+                  }}
+                />
+                <Button variant="contained" component="span">
+                  Upload File
+                </Button>
               </label>
             </Stack>
 
-            <Button type='submit' variant='contained' sx={{ mt: 3, mb: 2, px: 5 }} color="error">Submit</Button>
-            {error.status ? <Alert severity={error.type}>{error.msg}</Alert> : ''}
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ mt: 3, mb: 2, px: 5 }}
+              color="error"
+            >
+              Submit
+            </Button>
+            {error.status ? (
+              <Alert severity={error.type}>{error.msg}</Alert>
+            ) : (
+              ""
+            )}
           </Box>
         </Grid>
 
         <Grid item xs={7}>
-          <Box display="flex" justifyContent="center" sx={{ backgroundColor: 'info.light', padding: 1 }}>
-            <Typography variant="h5" component="div" sx={{ fontWeight: 'bold', color: 'white' }}> List of Candidates</Typography>
+          <Box
+            display="flex"
+            justifyContent="center"
+            sx={{ backgroundColor: "info.light", padding: 1 }}
+          >
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{ fontWeight: "bold", color: "white" }}
+            >
+              {" "}
+              List of Candidates
+            </Typography>
           </Box>
-          
+
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
@@ -204,18 +363,24 @@ function App() {
               <TableBody>
                 {candidates?.map((candidate, i) => {
                   return (
-                    <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableRow
+                      key={i}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
                       <TableCell align="center">{candidate.name}</TableCell>
                       <TableCell align="center">{candidate.email}</TableCell>
                       <TableCell align="center">{candidate.dob}</TableCell>
                       <TableCell align="center">{candidate.state}</TableCell>
                       <TableCell align="center">{candidate.gender}</TableCell>
                       <TableCell align="center">{candidate.location}</TableCell>
-                      <TableCell align="center"><Avatar src={`http://127.0.0.1:8000/.${candidate.pimage}`} /></TableCell>
+                      <TableCell align="center">
+                        <Avatar
+                          src={`http://127.0.0.1:8000/.${candidate.pimage}`}
+                        />
+                      </TableCell>
                     </TableRow>
-                  )
-                })
-                }
+                  );
+                })}
               </TableBody>
             </Table>
           </TableContainer>
